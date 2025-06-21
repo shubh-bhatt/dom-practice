79e9b84bc80b95ab4abd1023dcc8b275cd55fd84
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-06-21 23:03:26 +0530

    random color generator

--- 02/script.js
+++ 02/script.js
@@ -1,1 +1,10 @@
-console.log("hi there");
+let btn = document.querySelector("button");
+let box = document.querySelector("#box");
+
+btn.addEventListener("click", function () {
+  let c1 = Math.floor(Math.random() * 256);
+  let c2 = Math.floor(Math.random() * 256);
+  let c3 = Math.floor(Math.random() * 256);
+
+  box.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`;
+});
