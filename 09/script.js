f9073cd3865188cb76a465b928e795bcee124a90
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-06-25 00:17:10 +0530

    Instagram like bubble

--- 09/script.js
+++ 09/script.js
@@ -1,5 +1,18 @@
+let box = document.querySelector("#box");
 let img = document.querySelector("img");
 
-img.addEventListener("bdlclick", function () {
-    
+img.addEventListener("dblclick", function () {
+  let imgheart = document.createElement("img");
+  imgheart.setAttribute("src", "/09/heart.png");
+  imgheart.style.height = "25px";
+  imgheart.style.position = "absolute";
+  imgheart.style.top = "50%";
+  imgheart.style.left = "50%";
+  imgheart.style.transform = "translate(-50%, -50%)";
+
+  box.appendChild(imgheart);
+
+  setTimeout(function () {
+    imgheart.remove();
+  }, 500);
 });
