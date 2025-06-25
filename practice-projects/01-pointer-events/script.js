85c5599fc3eb0cc5e4df98b5c570d33a6fb5de76
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-06-26 00:30:40 +0530

    pointer events

---
+++ practice-projects/01-pointer-events/script.js
@@ -0,0 +1,16 @@
+let pointer = document.querySelector("#pointer");
+let body = document.querySelector("body");
+
+pointer.style.height = "20px";
+pointer.style.width = "20px";
+pointer.style.borderRadius = "50%";
+pointer.style.position = "absolute";
+pointer.style.backgroundColor = "white";
+
+body.addEventListener("mousemove", function (dets) {
+  pointer.style.top = `${dets.y}px`;
+  pointer.style.left = `${dets.x}px`;
+  pointer.style.transform = "translate(-50%, -50%)";
+
+  console.log(dets.x);
+});
